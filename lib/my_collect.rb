6a11fb33acea6090834c7a98907c25b58cbed9f7
Rick Rosetti <rick.rosetti@gmail.com>
2019-04-24 04:39:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(argument)
   i = 0
   collection = []
   while i<argument.length
-      collection << yield(i)
+      collection << yield(argument[i])
       i += 1
     end
     collection
